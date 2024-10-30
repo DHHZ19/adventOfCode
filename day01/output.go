bc37dd97425f1f8b82c9498ccd17ec99f00e5818
Diego Hernandez <62413339+DHHZ19@users.noreply.github.com>
2024-10-30 12:33:17 -0500

    remove useless comments in output.go

--- day01/output.go
+++ day01/output.go
@@ -6,12 +6,10 @@ import (
 )
 
 func main() {
-	// open file
 	f, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// remember to close the file at the end of the program
 	defer f.Close()
-
+	/// codd to be continued
 }
