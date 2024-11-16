0999fd2738a1753fdd2bbb451dccb3aec6c3be54
Diego Hernandez <diegofootball19@gmail.com>
2024-11-15 22:54:49 -0600

    start part 3

--- day03/solution01.js
+++ day03/solution01.js
@@ -1,6 +1,41 @@
 import fs from 'fs';
 
 // north (^), south (v), east (>), or west (<)
+const hash = {0: true}
+let x = 0
+let deliveredHouses = 1
 fs.readFile('./input.txt', 'utf8', (err, data) => {
+  let s = data.split("")
+
+  s.forEach(x => {
+    switch(x){
+      case '^':
+        if (!hash[x++]){
+          deliveredHouses++;
+        }
+        break
+      case 'v':
+        if (!hash[x++]){
+          deliveredHouses++;
+        }
+        x--
+        break
+      case '>':
+        if (!hash[x++]){
+          deliveredHouses++;
+        }
+        x++
+        break
+      case '<':
+        if (!hash[x++]){
+          deliveredHouses++;
+        }
+        x--
+        break
+    }
+
+   console.log(deliveredHouses)
+  })
 
 })
+
