d87f67803c383afbf63662fdb2a3f3b8a27af22c
Diego Hernandez <diegofootball19@gmail.com>
2024-11-19 21:37:31 -0600

    run prettier on everything

--- day01/solution01.js
+++ day01/solution01.js
@@ -2,27 +2,27 @@ import fs from "fs";
 import readline from "readline";
 
 const rl = readline.createInterface({
-    input: fs.createReadStream("input.txt"),
-    crlfDelay: Infinity,
+  input: fs.createReadStream("input.txt"),
+  crlfDelay: Infinity,
 });
 
-let floor = 0
+let floor = 0;
 rl.on("line", (line) => {
-    for (let i = 0; i < line.length; i++) {
-        if (line[i] === ")"){
-           floor--
-            if(floor === -1){
-                console.log(i, "winner")
-                break
-            }
-        } else if (line[i] === "("){
-            floor++
-            if(floor === -1){
-                console.log(i, "winner")
-                break
-            }
-        }
+  for (let i = 0; i < line.length; i++) {
+    if (line[i] === ")") {
+      floor--;
+      if (floor === -1) {
+        console.log(i, "winner");
+        break;
+      }
+    } else if (line[i] === "(") {
+      floor++;
+      if (floor === -1) {
+        console.log(i, "winner");
+        break;
+      }
     }
+  }
 
-    console.log(floor)
-})
+  console.log(floor);
+});
--- day05/solution02.js
+++ day05/solution02.js
@@ -1,7 +1,6 @@
 import fs from "fs";
 import readline from "readline";
 
-
 const rl = readline.createInterface({
   input: fs.createReadStream("input.txt"),
   crlfDelay: Infinity,
@@ -10,22 +9,22 @@ const rl = readline.createInterface({
 let res = 0;
 
 rl.on("line", (line) => {
-  let flag1 = false
-  let flag2 = false
+  let flag1 = false;
+  let flag2 = false;
   for (let i = 0; i < line.length; i++) {
-    let substr = line[i] + line[i + 1]
-    if(line.includes(substr,i + 2)){
-      flag1 = true
+    let substr = line[i] + line[i + 1];
+    if (line.includes(substr, i + 2)) {
+      flag1 = true;
     }
 
     if (i >= 0 && line[i] === line[i + 2]) {
-     flag2 = true
+      flag2 = true;
     }
   }
 
-  if(flag1 && flag2){
-      res++
+  if (flag1 && flag2) {
+    res++;
   }
 
-  console.log(res)
-})
+  console.log(res);
+});
